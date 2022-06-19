c70bee28693c16be242e9ca97c1b12842874d2a1
ant-currenxie <anthony.chan@currenxie.com>
2022-06-19 15:09:33 +0800

    install apisauce

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,31 +1,41 @@
-import { StyleSheet, View, Text } from 'react-native'
-
-import EditScreenInfo from '../components/EditScreenInfo'
+import { Button } from '@starter/component/Button'
+import { FormText } from '@starter/component/Form/FormText'
+import { useState } from 'react'
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native'
 import { RootTabScreenProps } from '../types'
 
 export default function MainScreen({ navigation }: RootTabScreenProps<'Main'>) {
+  const [instrument, setInstrument] = useState('')
+  const onPressSearch = () => {
+    console.log(instrument)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} />
-      <EditScreenInfo path='/screens/TabOneScreen.tsx' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.searchBox}>
+          <View style={styles.input}>
+            <FormText title='instrument' onChangeText={setInstrument} placeHolder='Please enter here...' />
+          </View>
+          <Button title='Search' onPress={onPressSearch} />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  input: {
+    flex: 1,
+    marginRight: 20,
+  },
+  searchBox: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
   },
 })
